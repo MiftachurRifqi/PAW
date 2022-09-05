24d0f35a5e2c8d497b790d258415adb5c8de1dfb
MiftachurRifqi <101687030+MiftachurRifqi@users.noreply.github.com>
2022-09-05 21:13:23 +0700

    Delete tujuan_session.php

--- tujuan_session.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    echo "File 2 - Session <br>";
-    echo "Nama: ".$_SESSION['nama'];
-    echo "<br> NIM: ".$_SESSION['nim'];
-    echo "<br> Prodi: ".$_SESSION['prodi'];
-    echo "<br> Hobi: ".$_SESSION['hobi'];
-?>
