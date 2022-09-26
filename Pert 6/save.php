1b7524a5ca822216083a84338273011fe9c551f8
Syibi <98193148+Syibi@users.noreply.github.com>
2022-09-26 22:36:28 +0700

    menyimpan data hasil input form

--- Pert 6/save.php
+++ Pert 6/save.php
@@ -2,12 +2,12 @@
 include "koneksi.php";
 
 $id_mhs=$_POST["id_mhs"];
+$id_prodi=$_POST["id_prodi"];
 $nama_mhs =$_POST["nama_mhs"];
 $alamat_mhs=$_POST["alamat_mhs"];
-$prodi_mhs=$_POST["prodi_mhs"];
 
-$sql="INSERT INTO tbl_mhs, tbl_prodi(id_mhs,nama_mhs,alamat_mhs,prodi_mhs)
-VALUES('$id_mhs','$nama_mhs','$alamat_mhs','$prodi_mhs')";
+$sql="INSERT INTO tbl_mhs (id_mhs, id_prodi, nama_mhs, alamat_mhs)
+VALUES('$id_mhs', '$id_prodi', '$nama_mhs','$alamat_mhs')";
 $query=mysqli_query($koneksi,$sql);
 
 if ($query) {
