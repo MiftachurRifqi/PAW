2fdf0de49dc41f8f9cb38cc9133e9af46c24cba4
M. Miftachur Rifqi Al H <101687030+MiftachurRifqi@users.noreply.github.com>
2022-10-20 15:41:57 +0700

    Delete Tugas P6 directory

--- Tugas P6/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "","pawdatabase");
-$sql = "SELECT * FROM tbl_mhs, tbl_prodi";
-$hasil = mysqli_query($koneksi, $sql);
-while($baris = mysqli_fetch_assoc($hasil)){
-}
-?>
